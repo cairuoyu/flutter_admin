71ed23607993b5a661d43fc85591568c9b0d2d50
cairuoyu <cry_cairuoyu@163.com>
2020-07-28 17:13:32 +0800

    封装表格树组件

---
+++ lib/components/treeTable.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_admin/vo/treeVO.dart';
+
+class TreeTableColumnData {
+  TreeTableColumnData(this.label, this.getData, {this.width = 200});
+  Function getData;
+  String label;
+  double width;
+}
+
+class TreeTable<T extends TreeData> extends StatefulWidget {
+  TreeTable({
+    Key key,
+    this.columnData,
+    this.data,
+    this.toolbars,
+    this.getRowOper,
+    this.onSelected,
+    this.width = 1000,
+    this.tableWidth = 1000,
+  }) : super(key: key);
+  final List<TreeTableColumnData> columnData;
+  final List<TreeVO<T>> data;
+  final Widget toolbars;
+  final Function getRowOper;
+  final Function onSelected;
+  final double width;
+  final double tableWidth;
+
+  @override
+  _TreeTableState createState() => _TreeTableState<T>();
+}
+
+class _TreeTableState<T extends TreeData> extends State<TreeTable<T>> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Column c = Column(
+      children: [
+        _getToolbars(),
+        _getTableHeader(),
+        _getTableBody(),
+      ],
+    );
+    var s = SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        width: widget.tableWidth,
+        child: c,
+      ),
+    );
+
+    return Container(
+      child: ListView(
+        children: [s],
+      ),
+      width: widget.width,
+      decoration: _getBoxDecoration(),
+    );
+  }
+
+  _getToolbars() {
+    var result = Container(
+      decoration: _getBoxDecoration(),
+      child: widget.toolbars,
+    );
+    return result;
+  }
+
+  Widget _getTableHeader() {
+    var leading = Checkbox(
+        value: false,
+        onChanged: (v) {
+          setState(() {
+            // checkChildren(vo, v);
+          });
+        });
+    List<Widget> list = [];
+    widget.columnData.forEach((element) {
+      list.add(_getCell(element.label, width: element.width));
+    });
+    if (widget.getRowOper != null) {
+      list.insert(0, _getCell('操作', width: 100));
+    }
+    var result = Container(
+      decoration: _getBoxDecoration(header: true),
+      child: ListTile(
+        leading: leading,
+        title: Row(
+          children: list,
+        ),
+      ),
+    );
+    return result;
+  }
+
+  Widget _getTableBody() {
+    var rowList = _getRowList(widget.data ?? [], null);
+    return Column(
+      children: rowList,
+    );
+  }
+
+  List<Widget> _getRowList(List<TreeVO<T>> data, TreeVO<T> parent) {
+    List<Widget> list = [];
+    for (var i = 0; i < data.length; i++) {
+      TreeVO<T> vo = data[i];
+      List<Widget> columnList = [];
+      columnList = widget.columnData
+          .map<Widget>(
+            (v) => _getCell(v.getData(vo.data), width: v.width),
+          )
+          .toList();
+
+      if (widget.getRowOper != null) {
+        var s = SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            width: 100,
+            child: Row(
+              children: widget.getRowOper(vo, parent),
+            ),
+          ),
+        );
+        columnList.insert(0, s);
+      }
+
+      var title = Row(children: columnList);
+      var leading = Checkbox(
+          value: vo.checked,
+          onChanged: (v) {
+            setState(() {
+              vo.checked = v;
+              if (widget.onSelected != null) {
+                widget.onSelected(_getSelected(widget.data));
+              }
+              // _checkChildren(vo, v);
+            });
+          });
+      Widget d;
+      if (vo.children != null && vo.children.length > 0) {
+        d = Container(
+          decoration: _getBoxDecoration(),
+          child: ExpansionTile(
+            leading: leading,
+            initiallyExpanded: true,
+            children: _getRowList(vo.children, vo),
+            title: title,
+          ),
+        );
+      } else {
+        d = Container(
+          decoration: _getBoxDecoration(),
+          child: ListTile(
+            leading: leading,
+            title: title,
+          ),
+        );
+      }
+      list.add(d);
+    }
+    return list;
+  }
+
+  List<TreeVO<T>> _getSelected(List<TreeVO<T>> data) {
+    if (data == null) {
+      return [];
+    }
+    var selected = List<TreeVO<T>>();
+    data.forEach((element) {
+      if (element.checked) {
+        selected.add(element);
+      }
+      if (element.children != null && element.children.length > 0) {
+        selected.addAll(_getSelected(element.children));
+      }
+    });
+    return selected;
+  }
+
+  _getBoxDecoration({header: false, odd: true}) {
+    var bd = BoxDecoration(
+      boxShadow: [
+        BoxShadow(color: Colors.black26, offset: Offset(2.0, 2.0), blurRadius: 4.0),
+      ],
+      color: header ? Colors.white60 : odd ? Colors.white : Colors.white38,
+      border: Border(bottom: BorderSide(color: Colors.black12)),
+    );
+    return bd;
+  }
+
+  _getCell(title, {width: 200.0}) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Text(title),
+      width: width,
+    );
+  }
+  // _checkChildren(TreeVO vo, bool v) {
+  //   vo.checked = v;
+  //   if (vo.children != null) {
+  //     vo.children.forEach((c) {
+  //       c.checked = v;
+  //       _checkChildren(c, v);
+  //     });
+  //   }
+  // }
+}
--- lib/pages/menu/menuMenu.dart
+++ lib/pages/menu/menuMenu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_admin/api/menuDemoApi.dart';
 import 'package:flutter_admin/components/cryButton.dart';
 import 'package:flutter_admin/components/cryDialog.dart';
+import 'package:flutter_admin/components/treeTable.dart';
 import 'package:flutter_admin/generated/l10n.dart';
 import 'package:flutter_admin/models/menu.dart';
 import 'package:flutter_admin/vo/treeVO.dart';
@@ -17,6 +18,7 @@ class MenuMenu extends StatefulWidget {
 }
 
 class _MenuMenuState extends State<MenuMenu> {
+  List<TreeVO<Menu>> selected = [];
   @override
   void initState() {
     super.initState();
@@ -24,199 +26,77 @@ class _MenuMenuState extends State<MenuMenu> {
 
   @override
   Widget build(BuildContext context) {
-    Column c = Column(
-      children: [
-        _getToolbars(),
-        _getTableHeader(),
-        _getTableBody(),
-      ],
-    );
-    var s = SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        width: 1100,
-        child: c,
-      ),
-    );
-
-    return Container(
-      child: ListView(
-        children: [s],
-      ),
-      width: widget.expand ? double.infinity : 400,
-      decoration: _getBoxDecoration(),
-    );
-  }
-
-  _getToolbars() {
-    var selected = _getSelected(widget.treeVOList);
-    var result = Container(
-      decoration: _getBoxDecoration(),
-      child: ListTile(
-        title: ButtonBar(
-          alignment: MainAxisAlignment.start,
-          children: <Widget>[
-            CryButton(
-              iconData: Icons.add,
-              label: '添加菜单',
-              onPressed: () => widget.onEdit(Menu()),
-            ),
-            CryButton(
-              iconData: Icons.delete,
-              label: '删除',
-              onPressed: selected.length >= 1
-                  ? () {
-                      cryConfirm(context, S.of(context).confirmDelete, () async {
-                        List ids = selected.map((e) => e.data.id).toList();
-                        await MenuDemoApi.removeByIds(ids);
-                        widget.reloadData();
-                        Navigator.of(context).pop();
-                      });
-                    }
-                  : null,
-            ),
-          ],
-        ),
-      ),
+    List<TreeTableColumnData> columnData = [
+      TreeTableColumnData('名称', (Menu v) => v.name, width: 250),
+      TreeTableColumnData('英文名', (Menu v) => v.nameEn),
+      TreeTableColumnData('URL', (Menu v) => v.url),
+      TreeTableColumnData('备注', (Menu v) => v.remark, width: 300),
+    ];
+    var result = TreeTable(
+      columnData: columnData,
+      data: widget.treeVOList,
+      toolbars: _getToolbars(),
+      onSelected: (v) => _onSelected(v),
+      getRowOper: (TreeVO<Menu> v, TreeVO<Menu> parent) => _getRowOper(v, parent),
+      tableWidth: 1300,
+      width: widget.expand ? 1300 : 400,
     );
     return result;
   }
 
-  Widget _getTableHeader() {
-    var leading = Checkbox(
-        value: false,
-        onChanged: (v) {
-          setState(() {
-            // checkChildren(vo, v);
-          });
-        });
-    List<Widget> list = [
-      SizedBox(
-        width: 100,
-      ),
-      _getCell("名称"),
-    ];
-    list.add(_getCell("英文名"));
-    list.add(_getCell("URL"));
-    list.add(_getCell("备注"));
-    var d = Container(
-      decoration: _getBoxDecoration(header: true),
-      child: ListTile(
-        leading: leading,
-        title: Row(
-          children: list,
+  _getToolbars() {
+    var result = ButtonBar(
+      alignment: MainAxisAlignment.start,
+      children: <Widget>[
+        CryButton(
+          iconData: Icons.add,
+          label: '添加菜单',
+          onPressed: () => widget.onEdit(Menu()),
         ),
-      ),
-    );
-    return d;
-  }
-
-  Widget _getTableBody() {
-    var rowList = _getRowList(widget.treeVOList ?? [], null);
-    return Column(
-      children: rowList,
-    );
-  }
-
-  List<Widget> _getRowList(List<TreeVO<Menu>> data, TreeVO<Menu> parent) {
-    List<Widget> list = [];
-    for (var i = 0; i < data.length; i++) {
-      TreeVO<Menu> vo = data[i];
-      List<Widget> columnList = [];
-      columnList.add(
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => widget.onEdit(Menu(pname: vo.data.name, pid: vo.data.id)),
+        CryButton(
+          iconData: Icons.delete,
+          label: '删除',
+          onPressed: selected.length >= 1
+              ? () {
+                  cryConfirm(context, S.of(context).confirmDelete, () async {
+                    List ids = selected.map((e) => e.data.id).toList();
+                    await MenuDemoApi.removeByIds(ids);
+                    widget.reloadData();
+                    Navigator.of(context).pop();
+                  });
+                }
+              : null,
         ),
-      );
-      columnList.add(
-        IconButton(
-          icon: Icon(Icons.edit),
-          onPressed: () {
-            Menu menu = vo.data;
-            if (parent != null) {
-              menu.pname = parent.data.name;
-            }
-            widget.onEdit(menu);
-          },
-        ),
-      );
-
-      columnList.add(_getCell(vo.data.name));
-      columnList.add(_getCell(vo.data.nameEn ?? '--'));
-      columnList.add(_getCell(vo.data.url ?? '--'));
-      columnList.add(_getCell(vo.data.remark ?? '--'));
-      var title = Row(children: columnList);
-      var leading = Checkbox(
-          value: vo.checked,
-          onChanged: (v) {
-            setState(() {
-              vo.checked = v;
-              // _checkChildren(vo, v);
-            });
-          });
-      var d;
-      if (vo.children != null && vo.children.length > 0) {
-        d = Container(
-          decoration: _getBoxDecoration(),
-          child: ExpansionTile(
-            leading: leading,
-            initiallyExpanded: true,
-            children: _getRowList(vo.children, vo),
-            title: title,
-          ),
-        );
-      } else {
-        d = Container(
-          decoration: _getBoxDecoration(),
-          child: ListTile(
-            leading: leading,
-            title: title,
-          ),
-        );
-      }
-      list.add(d);
-    }
-    return list;
-  }
-
-  _getCell(title, {width: 200.0}) {
-    return Container(padding: EdgeInsets.symmetric(horizontal: 20), child: Text(title), width: width);
-  }
-
-  List<TreeVO<Menu>> _getSelected(List<TreeVO<Menu>> data) {
-    if (data == null) {
-      return [];
-    }
-    var selected = List<TreeVO<Menu>>();
-    data.forEach((element) {
-      if (element.checked) {
-        selected.add(element);
-      }
-      if (element.children != null && element.children.length > 0) {
-        selected.addAll(_getSelected(element.children));
-      }
-    });
-    return selected;
-  }
-
-  _getBoxDecoration({header: false, odd: true}) {
-    var bd = BoxDecoration(
-      boxShadow: [
-        BoxShadow(color: Colors.black26, offset: Offset(2.0, 2.0), blurRadius: 4.0),
       ],
-      color: header ? Colors.white60 : odd ? Colors.white : Colors.white38,
-      border: Border(bottom: BorderSide(color: Colors.black12)),
     );
-    return bd;
+    return result;
   }
-  // _checkChildren(TreeVO vo, bool v) {
-  //   vo.checked = v;
-  //   if (vo.children != null) {
-  //     vo.children.forEach((c) {
-  //       c.checked = v;
-  //       _checkChildren(c, v);
-  //     });
-  //   }
-  // }
+
+  List<Widget> _getRowOper(TreeVO<Menu> vo, TreeVO<Menu> parent) {
+    List<Widget> columnList = [];
+    Menu menu = vo.data;
+    columnList.add(
+      IconButton(
+        icon: Icon(Icons.add),
+        onPressed: () => widget.onEdit(Menu(pname: vo.data.name, pid: vo.data.id)),
+      ),
+    );
+    columnList.add(
+      IconButton(
+        icon: Icon(Icons.edit),
+        onPressed: () {
+          if (parent != null) {
+            menu.pname = parent.data.name;
+          }
+          widget.onEdit(menu);
+        },
+      ),
+    );
+    return columnList;
+  }
+
+  _onSelected(v) {
+    selected = v;
+    setState(() {});
+  }
 }
