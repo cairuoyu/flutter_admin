a2a8a699f905477861294a841ded43cf3fa528e3
cairuoyu <cry_cairuoyu@163.com>
2021-02-23 13:38:04 +0800

    图表-人员统计-增加柱状图、配置

--- lib/constants/enum.dart
+++ lib/constants/enum.dart
@@ -2,3 +2,4 @@ enum MenuDisplayType { drawer, side }
 enum OperationType { add, edit, delete }
 enum TabMenuOption { close, closeAll, closeOthers, closeAllToTheRight, closeAllToTheLeft }
 enum ChartTypeCircular { pie, doughnut, radialBar }
+enum ChartTypeCartesian { line, column, stackedColumn }
