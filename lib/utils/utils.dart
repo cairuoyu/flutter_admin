65f9935770010313a2b618b3505708ba64bd0196
cairuoyu <cry_cairuoyu@163.com>
2021-06-22 09:56:20 +0800

    增加注释：http://cairuoyu.com、https://github.com/cairuoyu/flutter_admin

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,6 @@
 /// @author: cairuoyu
-/// @Copyright: https://github.com/cairuoyu/flutter_admin
+/// @homepage: http://cairuoyu.com
+/// @github: https://github.com/cairuoyu/flutter_admin
 /// @date: 2021/6/21
 /// @version: 1.0
 /// @description: 常用工具类
