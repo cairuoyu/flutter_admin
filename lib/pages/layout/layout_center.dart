519a2dd9099d8c8c00d553370a1a38d64a41f7f3
cairuoyu <cry_cairuoyu@163.com>
2021-08-17 17:54:02 +0800

    重构：弃用TabBar和TabBarView构建布局 https://github.com/cairuoyu/flutter_admin

--- lib/pages/layout/layout_center.dart
+++ lib/pages/layout/layout_center.dart
@@ -1,20 +1,9 @@
-/// @author: cairuoyu
-/// @homepage: http://cairuoyu.com
-/// @github: https://github.com/cairuoyu/flutter_admin
-/// @date: 2021/6/21
-/// @version: 1.0
-/// @description:
-
-import 'package:cry/cry_all.dart';
-import 'package:cry/cry_menu.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin/common/routes.dart';
-import 'package:flutter_admin/constants/enum.dart';
 import 'package:flutter_admin/models/tab_page.dart';
 import 'package:flutter_admin/pages/common/keep_alive_wrapper.dart';
 import 'package:flutter_admin/pages/layout/layout_controller.dart';
-import 'package:flutter_admin/pages/layout/layout_menu_controller.dart';
 import 'package:flutter_admin/utils/store_util.dart';
 import 'package:flutter_admin/utils/utils.dart';
 import 'package:get/get.dart';
@@ -27,9 +16,6 @@ class LayoutCenter extends StatefulWidget {
 }
 
 class LayoutCenterState extends State<LayoutCenter> with TickerProviderStateMixin {
-  Container content = Container();
-  TabController? tabController;
-
   @override
   void initState() {
     super.initState();
@@ -37,29 +23,23 @@ class LayoutCenterState extends State<LayoutCenter> with TickerProviderStateMixi
 
   @override
   Widget build(BuildContext context) {
+    print('layoutCenter--build');
     var openedTabPageList = StoreUtil.readOpenedTabPageList();
     var currentOpenedTabPageId = StoreUtil.readCurrentOpenedTabPageId();
-    int preIndex = tabController?.index ?? 0;
-    int initialIndex = preIndex < openedTabPageList.length ? preIndex : 0;
-    tabController = TabController(vsync: this, length: openedTabPageList.length, initialIndex: initialIndex);
-    tabController!.addListener(() {
-      if (tabController!.indexIsChanging) {
-        StoreUtil.writeCurrentOpenedTabPageId(openedTabPageList[tabController!.index]!.id);
-        LayoutMenuController controller = Get.find();
-        controller.update();
-      }
-    });
-    int index = openedTabPageList.indexWhere((note) => note!.id == currentOpenedTabPageId);
-    tabController!.animateTo(index);
+    int currentIndex = openedTabPageList.indexWhere((note) => note!.id == currentOpenedTabPageId);
 
-    TabBar tabBar = TabBar(
-      controller: tabController,
-      isScrollable: true,
-      indicator: const UnderlineTabIndicator(),
-      tabs: openedTabPageList.map<Tab>((TabPage? tabPage) {
+    var tabBar = Row(
+      children: openedTabPageList.map((TabPage? tabPage) {
         var tabContent = Row(
           children: <Widget>[
-            Text(Utils.isLocalEn(context) ? tabPage!.nameEn ?? '' : tabPage!.name ?? ''),
+            TextButton(
+              onPressed: () {
+                StoreUtil.writeCurrentOpenedTabPageId(tabPage!.id);
+                setState(() {});
+              },
+              child: Text(Utils.isLocalEn(context) ? tabPage!.nameEn ?? '' : tabPage!.name ?? ''),
+              style: ButtonStyle(foregroundColor: MaterialStateProperty.all(currentOpenedTabPageId == tabPage.id ? Colors.red : Colors.yellow)),
+            ),
             if (!Routes.defaultTabPage.contains(tabPage))
               Material(
                 type: MaterialType.transparency,
@@ -75,73 +55,18 @@ class LayoutCenterState extends State<LayoutCenter> with TickerProviderStateMixi
               )
           ],
         );
-        return Tab(
-          child: CryMenu(
-            child: tabContent,
-            onSelected: (dynamic v) {
-              switch (v) {
-                case TabMenuOption.close:
-                  Utils.closeTab(tabPage);
-                  break;
-                case TabMenuOption.closeAll:
-                  Utils.closeAllTab();
-                  break;
-                case TabMenuOption.closeOthers:
-                  Utils.closeOtherTab(tabPage);
-                  break;
-                case TabMenuOption.closeAllToTheRight:
-                  Utils.closeAllToTheRightTab(tabPage);
-                  break;
-                case TabMenuOption.closeAllToTheLeft:
-                  Utils.closeAllToTheLeftTab(tabPage);
-                  break;
-              }
-            },
-            itemBuilder: (context) => <PopupMenuEntry<TabMenuOption>>[
-              if (!Routes.defaultTabPage.contains(tabPage))
-                PopupMenuItem(
-                  value: TabMenuOption.close,
-                  child: ListTile(
-                    title: Text('Close'),
-                  ),
-                ),
-              PopupMenuItem(
-                value: TabMenuOption.closeAll,
-                child: ListTile(
-                  title: Text('Close All'),
-                ),
-              ),
-              PopupMenuItem(
-                value: TabMenuOption.closeOthers,
-                child: ListTile(
-                  title: Text('Close Others'),
-                ),
-              ),
-              PopupMenuItem(
-                value: TabMenuOption.closeAllToTheRight,
-                child: ListTile(
-                  title: Text('Close All to the Right'),
-                ),
-              ),
-              PopupMenuItem(
-                value: TabMenuOption.closeAllToTheLeft,
-                child: ListTile(
-                  title: Text('Close All to the Left'),
-                ),
-              ),
-            ],
-          ),
-        );
+        return tabContent;
       }).toList(),
     );
 
-    content = Container(
+    var content = Container(
       child: Expanded(
-        child: TabBarView(
-          controller: tabController,
+        child: IndexedStack(
+          index: currentIndex,
           children: openedTabPageList.map((TabPage? tabPage) {
             var page = tabPage!.url != null ? Routes.layoutPagesMap[tabPage.url!] ?? Container() : tabPage.widget ?? Container();
-            return KeepAliveWrapper(child: page);
+            return KeepAliveWrapper(child: page,key: Key('pageid ${tabPage.id}'),);
+            // return page;
           }).toList(),
         ),
       ),
